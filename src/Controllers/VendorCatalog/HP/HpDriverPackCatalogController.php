a5e958648c56b5e78b73939e3aa04de33703bd10
Knut Leborg <knut.leborg@telecomputing.no>
2019-05-22 19:15:16 +0200

    add progress bar to driver pack processing

--- src/Controllers/VendorCatalog/HP/HpDriverPackCatalogController.php
+++ src/Controllers/VendorCatalog/HP/HpDriverPackCatalogController.php
@@ -4,6 +4,7 @@ namespace Klepak\DriverManagement\Controllers\VendorCatalog\HP;
 
 use Log;
 use Klepak\DriverManagement\Models\HP\HpDriverPack;
+use ConsoleProgressBar;
 
 /**
  * @resource HpDriverPack
@@ -32,12 +33,22 @@ class HpDriverPackCatalogController extends HpCatalogBaseController
     {
         Log::info("Process driver packs from driver pack catalog");
 
-        foreach($this->getLocalCatalog()->xpath("//ProductOSDriverPack") as $driverPack)
+        $driverPacks = $this->getLocalCatalog()->xpath("//ProductOSDriverPack");
+
+        $progress = (new ConsoleProgressBar)
+            ->max(count($driverPacks))
+            ->message('Processing driver packs');
+
+        $i = 0;
+        foreach($driverPacks as $driverPack)
         {
+            $progress
+                ->update(++$i);
+
             HpDriverPack::updateOrCreate(
                 [
                     "product_type" => (string)$driverPack->ProductType,
-                    "system_id" => (string)$driverPack->SystemId,
+                    "system_id" => explode(',', (string)$driverPack->SystemId),
                     "system_name" => (string)$driverPack->SystemName,
                     "os_name" => (string)$driverPack->OSName,
                 ],
@@ -46,6 +57,8 @@ class HpDriverPackCatalogController extends HpCatalogBaseController
                 ]
             );
         }
+
+        $progress->completed();
     }
 
     public function extractCatalog($catalogPath)
